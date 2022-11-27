272a2ff0916215f7661a37693a34441754db0028
OKitel <kitelolga@gmail.com>
2022-11-27 22:48:16 +0100

    feat: add app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Board from "./components/Board/Board";
+import { getMainDefinition } from "@apollo/client/utilities";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+  split,
+} from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const httpLink = new HttpLink({ uri: "http://localhost:4445/graphql" });
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4445/graphql",
+  })
+);
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpLink
+);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App" style={{ height: "100vh" }}>
+        <Board />
+      </div>
+    </ApolloProvider>
   );
 }
 
